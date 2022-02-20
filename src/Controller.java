7802246475ee2b0bab7954b6f9a512919072c808
dlin96 <dlin96@gatech.edu>
2022-02-20 01:00:53 -0500

    welcome screen with adjustments to controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,19 +1,55 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Controller class that starts the tower defense game when run.
+ *
+ */
 public class Controller extends Application{
     private static Stage window;
     private static Scene welcomeScreen, configScreen, gameScreen;
 
+    /**
+     * Start method that sets up the game when launched.
+     *
+     * @param primaryStage The window of the game.
+     */
     public void start(Stage primaryStage) {
         window = primaryStage;
-
+        WelcomeScreen welcomeScreenTemp = new WelcomeScreen();
+        welcomeScreen = welcomeScreenTemp.getScene();
         window.setTitle("Game Title");
-
+        window.setScene(welcomeScreen);
         window.show();
     }
 
+    /**
+     * Closes the window of the game when event activates.
+     *
+     */
+    public static void quitAction() {
+        window.close();
+    }
+
+    /**
+     * Changes scene to the configuration screen.
+     *
+     */
+    public static void startButton() {
+        InitialConfig configTemp = new InitialConfig();
+        configTemp.initialize();
+        configScreen = configTemp.getScene();
+        window.setScene(configScreen);
+        window.show();
+    }
+
+    /**
+     * Main method that launches the game.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
---
+++ src/WelcomeScreen.java
@@ -0,0 +1,63 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+/**
+ * Class that creates the title screen of the game
+ *
+ */
+public class WelcomeScreen {
+
+    // Declaring/Initializing variables
+    private BorderPane pane = new BorderPane();
+    private Text welcomeBanner = new Text("Team 104 Tower Defense Game");
+    private HBox topPane = new HBox();
+    private Button startButton = new Button("start");
+    private Button quitButton = new Button("quit");
+    private HBox centerPane = new HBox(100);
+    private Scene scene = new Scene(pane, 500, 300);
+
+    /**
+     * Constructor that puts together panes and buttons onto the screen
+     */
+    public WelcomeScreen() {
+        // setting panes and boxes
+        topPane.getChildren().add(welcomeBanner);
+        pane.setTop(topPane);
+        centerPane.getChildren().addAll(startButton, quitButton);
+        pane.setCenter(centerPane);
+
+        // Aligning buttons and title
+        topPane.setAlignment(Pos.BOTTOM_CENTER);
+        centerPane.setAlignment(Pos.CENTER);
+
+        quitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Controller.quitAction();
+            }
+        });
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Controller.startButton();
+            }
+        });
+    }
+
+    /**
+     * Returns the default welcome screen
+     *
+     * @return The welcome screen scene
+     */
+    public Scene getScene() {
+        return scene;
+    }
+
+}
