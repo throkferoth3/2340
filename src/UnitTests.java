07baaf1e8861557c93a693df3598e5f24ad6f55a
dlin96 <dlin96@gatech.edu>
2022-03-06 12:14:41 -0500

    create UnitTest class template

---
+++ src/UnitTests.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitTests {
+
+    private static final int TIMEOUT = 200;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void exampleTest() {
+
+    }
+}
