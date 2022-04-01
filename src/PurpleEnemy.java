c0fe14ed909a05b1a3b0b134b70382e1e95dbe12
Ngo, Steven V <sngo30@gatech.edu>
2022-04-01 02:02:14 -0400

    Update PurpleEnemy.java
    added damage

--- src/PurpleEnemy.java
+++ src/PurpleEnemy.java
@@ -5,6 +5,7 @@ import javafx.scene.paint.Color;
  */
 public class PurpleEnemy extends Enemy {
     public PurpleEnemy() {
+        damage = 15;
         health = 20;
         getDisplay().setFill(Color.PURPLE);
     }
