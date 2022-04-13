a76c4b3a82994a09afadfc58bf4109eadef714c4
mwright301 <mwright301@gatech.edu>
2022-04-12 20:08:12 -0400

    added partial functionality for the red tower.

--- src/StartCombat.java
+++ src/StartCombat.java
@@ -6,9 +6,13 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.util.Duration;
 import javafx.scene.shape.Polyline;
+
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -36,9 +40,13 @@ public class StartCombat {
 
     public void startCombat() {
         setInCombat(true);
-        makeTimer().scheduleAtFixedRate(spawnEnemies(2, 5, 1, 0), 0, 1000);
-        makeTimer().scheduleAtFixedRate(spawnEnemies(2, 5, 1, 1), 2000, 1000);
-        makeTimer().scheduleAtFixedRate(spawnEnemies(2, 5, 1, 2), 4000, 1000);
+        makeTimer().scheduleAtFixedRate(spawnEnemies(1, 5, 1, 0), 0, 1000);
+        makeTimer().scheduleAtFixedRate(spawnEnemies(1, 5, 1, 0), 1000, 1000);
+        makeTimer().scheduleAtFixedRate(spawnEnemies(1, 5, 1, 1), 2000, 1000);
+        makeTimer().scheduleAtFixedRate(spawnEnemies(1, 5, 1, 1), 3000, 1000);
+        makeTimer().scheduleAtFixedRate(spawnEnemies(1, 5, 1, 2), 4000, 1000);
+        makeTimer().scheduleAtFixedRate(spawnEnemies(1, 5, 1, 2), 5000, 1000);
+        makeTimer().scheduleAtFixedRate(attackEnemies(), 0, 500);
         TimerTask buttonRespawn = new TimerTask() {
             private Enemy enemy;
             @Override
@@ -116,6 +124,55 @@ public class StartCombat {
             }
         };
         return enemySpawner;
+    }
+    public TimerTask attackEnemies() {
+        TimerTask enemyAttacker = new TimerTask() {
+            @Override
+            public void run() {
+
+                    ArrayList<Integer> arr = new ArrayList<Integer>();
+                    for (Tower t : PlayerInfo.getTowerList()) {
+                        for (Integer i : PlayerInfo.getEnemyMap().keySet()) {
+                            Enemy e = PlayerInfo.getEnemyMap().get(i);
+                            if (t.getPosX() - e.getX() <= 20 && t.getPosX() - e.getX() >= -20) {
+                                if (t instanceof RedTower) {
+                                    Platform.runLater(() -> {
+                                        Circle bullet = new Circle();
+                                        bullet.setFill(Color.RED);
+                                        bullet.setRadius(5);
+                                        bullet.setCenterX(t.getPosX());
+                                        bullet.setCenterY(t.getPosY());
+                                        MainGame.getCenter().getChildren().add(bullet);
+                                        PathTransition transition = new PathTransition();
+                                        transition.setNode(bullet);
+                                        transition.setDuration(Duration.millis(100));
+                                        Line line = new Line();
+                                        line.setStartX(t.getPosX());
+                                        line.setStartY(t.getPosY());
+                                        line.setEndX(e.getX());
+                                        line.setEndY(e.getY());
+                                        transition.setPath(line);
+                                        transition.setCycleCount(1);
+                                        transition.setInterpolator(Interpolator.LINEAR);
+                                        transition.setOnFinished(actionEvent -> {
+                                            MainGame.getCenter().getChildren().remove(bullet);
+                                        });
+                                        transition.play();
+                                        while (Platform.isNestedLoopRunning()) {
+                                            continue;
+                                        }
+                                    });
+                                }
+                                arr.add(e.getId());
+                            }
+                        }
+                    }
+                    for (Integer i : arr) {
+                        PlayerInfo.getEnemyMap().remove(i);
+                    }
+            }
+        };
+        return enemyAttacker;
     }
     public static void setInCombat(boolean c) {
         inCombat = c;
