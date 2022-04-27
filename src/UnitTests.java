be064074a7210594b4e393d463589e2e38f16ee7
Ngo, Steven V <sngo30@gatech.edu>
2022-04-26 23:18:43 -0400

    Update UnitTests.java
    fixed testGreenTowerAddHealthUpgrade()

--- src/UnitTests.java
+++ src/UnitTests.java
@@ -342,8 +342,8 @@ public class UnitTests {
         PlayerInfo.setDifficulty(0);
         PlayerInfo.initHealthAndMoney();
         Tower g = new GreenTower(1.0, 1.0);
-        g.upgrade();
-        g.addHealth();
+        (GreenTower)g.upgrade();
+        (GreenTower)g.addHealth();
         assertEquals(22, PlayerInfo.getHealth());
     }
 }
